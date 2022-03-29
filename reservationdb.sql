87677c18effb237bc459b3af477a7e127fc4f26f
Tejas Tawde <tejascodes.281@gmail.com>
2022-03-29 15:44:06 +0530

    Initial Commit

---
+++ reservationdb.sql
@@ -0,0 +1,61 @@
+CREATE TABLE PASSENGER
+(PNR VARCHAR(20) NOT NULL PRIMARY KEY, 
+first_name VARCHAR(15) NOT NULL,
+last_name VARCHAR(15),
+age INT NOT NULL,
+gender VARCHAR(5),
+phone_num VARCHAR(10) NOT NULL,
+reserve_status VARCHAR(10) NOT NULL,
+FOREIGN KEY (receipt_num) REFERENCES FARE(receipt_num),
+FOREIGN KEY (train_num) REFERENCES TRAIN(train_num),
+FOREIGN KEY (class_num) REFERENCES CLASS(class_num)
+);
+
+CREATE TABLE STATION
+(
+stn_code VARCHAR(6) NOT NULL PRIMARY KEY,
+stn_name VARCHAR(15) NOT NULL,
+stn_type VARCHAR(10) NOT NULL,		#Station type: Terminus, Junction, Central, Station etc.
+);
+
+CREATE TABLE TRAIN
+(
+train_num NUMERIC(5) NOT NULL PRIMARY KEY,
+train_name VARCHAR(50) NOT NULL,
+train_src VARCHAR(15) NOT NULL,
+train_dstn VARCHAR(15) NOT NULL,
+coach_qty INT NOT NULL
+);
+
+CREATE TABLE TICKET
+(
+ticket_num NUMERIC(6) NOT NULL PRIMARY KEY,
+arrivalTime NUMERIC(4),
+departureTime NUMERIC(4)
+ticket_type VARCHAR(10)
+);
+
+CREATE TABLE FARE
+(
+receipt_num NUMERIC(5) PRIMARY KEY,
+amount NUMERIC,
+FOREIGN KEY (PNR) REFERENCES PASSENGER(PNR)
+);
+
+CREATE TABLE CLASS
+(
+class_num INT PRIMARY KEY
+seat_qty INT,
+);
+
+CREATE TABLE BOOKS
+(
+FOREIGN KEY (PNR) REFERENCES PASSENGER(PNR),
+FOREIGN KEY (ticket_num) REFERENCES TICKET(ticket_num)
+);
+
+CREATE TABLE STOPS_AT
+(
+FOREIGN KEY (train_num) REFERENCES TRAIN(train_num),
+FOREIGN KEY (stn_code) REFERENCES STATION(stn_code)
+);
